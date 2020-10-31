d26f65109cfd34d92d3134367415e7670bd37a87
Mehebub-new <73712771+Mehebub-new@users.noreply.github.com>
2020-10-31 11:15:22 +0530

    Update and rename README.md to ASCII values of charecters.c

---
+++ ASCII values of charecters.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{int i;
+for(i=50; i<=70; i++)
+{printf("The ASCII Value of charecter  %c=%d\n",i,i);
+}
+return 0;
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# linked-list
-To learn different link list patterns
